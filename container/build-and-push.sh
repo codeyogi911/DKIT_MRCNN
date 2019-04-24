af25c02dbe276debabd8cd47e2fc7466e0a393a5
Shashwat Jain <shashwatjain@icloud.com>
2019-04-24 17:45:40 +0800

    intialize

---
+++ container/build-and-push.sh
@@ -0,0 +1,38 @@
+%%sh
+
+# The name of our algorithm
+algorithm_name=dkit_mask_mcrnn
+
+cd container
+
+chmod +x ${algorithm_name}/train
+chmod +x ${algorithm_name}/serve
+
+account=$(aws sts get-caller-identity --query Account --output text)
+
+# Get the region defined in the current configuration (default to us-west-2 if none defined)
+region=$(aws configure get region)
+# specifically setting to us-east-2 since during the pre-release period, we support only that region.
+# region=${region:-us-east-2}
+
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:latest"
+
+# If the repository doesn't exist in ECR, create it.
+
+aws ecr describe-repositories --repository-names "${algorithm_name}" > /dev/null 2>&1
+
+if [ $? -ne 0 ]
+then
+    aws ecr create-repository --repository-name "${algorithm_name}" > /dev/null
+fi
+
+# Get the login command from ECR and execute it directly
+$(aws ecr get-login --region ${region} --no-include-email)
+
+# Build the docker image locally with the image name and then push it to ECR
+# with the full name.
+
+docker build  -t ${algorithm_name} .
+docker tag ${algorithm_name} ${fullname}
+
+docker push ${fullname}
